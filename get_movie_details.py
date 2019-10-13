1f943a7d52df64766d1d8e0d02651036b121c459
Shaun Schembri <shaun.schembri@gmail.com>
2019-10-13 18:28:22 +0200

    Added option to output result in JSON

--- get_movie_details.py
+++ get_movie_details.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 import argparse
 import logging
-import json
 import elastictmdb
+import json
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(message)s")
 
-#Initialise ElasticTMDB
+# Initialise ElasticTMDB
 ElasticTMDB = elastictmdb.ElasticTMDB()
 
-#Parse command line arguments
+# Parse command line arguments
 argParser = argparse.ArgumentParser()
 argParser._action_groups.pop()
 required = argParser.add_argument_group('required arguments')
@@ -19,6 +19,7 @@ optional.add_argument("-d", "--director", type=str, action='append', help="Name 
 optional.add_argument("-c", "--cast", action='append', type=str, help="Name of actor. Can be used more then once to specify more cast members")
 optional.add_argument("-y", "--year", type=int, help="Movie release year")
 optional.add_argument("-f", "--force", action="store_true", help="Force a search on TMDB before returning results")
+optional.add_argument("-j", "--json", action="store_true", help="Output result in JSON")
 
 args = argParser.parse_args()
 msg = {}
@@ -35,38 +36,39 @@ if args.force:
     msg["force"] = args.force
 
 def build_movie_description(tmdbResult):
-    if tmdbResult != None:
+    if tmdbResult:
         desc = []
         if 'cast' in tmdbResult["_source"]:
             desc.append("Cast: " + ", ".join(tmdbResult["_source"]["cast"][:5]))
 
         if 'director' in tmdbResult["_source"]:
             desc.append("Director: " + ", ".join(tmdbResult["_source"]["director"]))
-        
+
         if 'rating' in tmdbResult["_source"]:
             desc.append("Rating: " + str(tmdbResult["_source"]["rating"]))
-        
+
         if 'description' in tmdbResult["_source"]:
-            desc.append("\n" + tmdbResult["_source"]["description"] +"\n")
+            desc.append("\n" + tmdbResult["_source"]["description"] + "\n")
 
         if 'year' in tmdbResult["_source"]:
             desc.append("Year: " + str(tmdbResult["_source"]["year"]))
-        
+
         if 'genre' in tmdbResult["_source"]:
             desc.append("Genre: " + ", ".join(tmdbResult["_source"]["genre"]))
-        
-       
+
         if 'country' in tmdbResult["_source"]:
             desc.append("Country: " + ", ".join(tmdbResult["_source"]["country"]))
-        
+
         if 'popularity' in tmdbResult["_source"]:
             desc.append("Popularity: " + str(round(tmdbResult["_source"]["popularity"], 1)))
 
         if '_score' in tmdbResult:
             desc.append("Score: " + str(round(tmdbResult["_score"], 1)))
-        
+
         return "\n".join(desc)
 
 result = ElasticTMDB.search_movie(msg)
-#print(json.dumps(ElasticTMDB.search_movie(msg), indent=1))
-print(build_movie_description(result))
+if args.json:
+    print(json.dumps(ElasticTMDB.search_movie(msg), indent=3))
+else:
+    print(build_movie_description(result))
